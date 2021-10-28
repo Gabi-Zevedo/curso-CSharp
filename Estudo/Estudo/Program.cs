df8bdf4accaac318add8ad9505df96f4f1df831d
Gabriel Azevedo <gsa384@gmail.com>
2021-10-28 11:44:53 -0300

    first commit

---
+++ Estudo/Estudo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("COÉ RAPAZEADA");
+        }
+    }
+}
